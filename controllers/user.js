40b5a74aa0b58fad0a9eb8178ee7f138537ec8ae
刘伯君 <liubj@wangsu.com>
2018-01-15 09:35:09 +0800

    添加验证码到session中用于验证

--- controllers/user.js
+++ controllers/user.js
@@ -23,118 +23,128 @@ const getSmsOption = (content) => {
     agent:false,
     rejectUnauthorized : false,
     headers:{
-    'Content-Type' : 'application/x-www-form-urlencoded',
-    'Content-Length' :content.length
+      'Content-Type' : 'application/x-www-form-urlencoded',
+      'Content-Length' : content.length
+    }
   }
-}
 
-const userController = {
+  const userController = {
 
-  update(req, resp, next) {
-    const userName = req.session.user.userName;
-    const body = req.body;
-    const id = req.params.id;
-    db.User.findById(id).then((user) => {
-      // 如果是当前用户,继续进行编辑
-      if(user.account === userName) {
-        const params = pickByAttrs(body, ['residence', 'name', 'email', 'phone', 'bus']);
-        user = assignIn(user, params);
-        return user.save();
+    update(req, resp, next) {
+      const userName = req.session.user.userName;
+      const body = req.body;
+      const id = req.params.id;
+      db.User.findById(id).then((user) => {
+        // 如果是当前用户,继续进行编辑
+        if(user.account === userName) {
+          const params = pickByAttrs(body, ['residence', 'name', 'email', 'phone', 'bus']);
+          user = assignIn(user, params);
+          return user.save();
+        }else {
+          resp.failed(FAILED_UPDATE_USER);
+        }
+      }).then(user => {
+        resp.success(user);
+      }).catch(next);
+    },
+
+    select(req, resp, next) {
+      const id = req.params.id;
+      db.User.findById(id).then((user) => {
+        resp.success(user);
+      }).catch(next);
+    },
+
+    create(req, resp, next) {
+      const body = req.body;
+      let findInvitor;
+      db.User.findOne({
+        phone: body.phone
+      }).then((user) => {
+        if(!user) {
+          if(req.session.validCode === body.code) {
+            const salt = bcrypt.genSaltSync(10);
+            const newUser = new db.User({
+              phoneNum: body.phone,
+              hash: bcrypt.hashSync(body.pwd, salt),
+            });
+            if(body.invitor) {
+              findInvitor = db.User.findById(body.invitor);
+            }
+            return Promise.all([newUser.save(), findInvitor]);
+          }else {
+            resp.failed('验证码不正确！');
+          }
+        }
+      }).then(([user, invitor]) => {
+        if(invitor) {
+          invitor.invited.push(user.id);
+          return Promise.all([user, invitor.save()]);
+        }else {
+          return Promise.all([user]);
+        }
+      }).then(([user]) => {
+        resp.success(`${user.name}用户创建成功！`);
+      }).catch(err => {
+        next(err);
+      });
+    },
+
+    login(req, resp, next) {
+      const body = req.body;
+      if(body.code === req.session.validCode) {
+        db.User.findOne({
+          name: body.name,
+        }).then(user => {
+          if(!user) {
+            resp.failed(`不存在的账户！`);
+          }else {
+            if(body.code === req.session.validCode && bcrypt.compareSync(body.pwd, user.hash)) {
+              req.session.user = user.toJSON();
+              resp.success('登录成功!');
+            }else {
+              resp.failed('密码不正确！');
+            }
+          }
+        }).catch(err => {
+          next(err);
+        })
       }else {
-        resp.failed(FAILED_UPDATE_USER);
+        resp.failed('验证码不正确！');
       }
-    }).then(user => {
-      resp.success(user);
-    }).catch(next);
-  },
+    },
 
-  select(req, resp, next) {
-    const id = req.params.id;
-    db.User.findById(id).then((user) => {
-      resp.success(user);
-    }).catch(next);
-  },
-
-  create(req, resp, next) {
-    const body = req.body;
-    let findInvitor;
-    db.User.findOne({
-      phone: body.phone
-    }).then((user) => {
-      if(!user) {
-        // TODO 校验码比较
-        const salt = bcrypt.genSaltSync(10);
-        const newUser = new db.User({
-          phoneNum: body.phone,
-          hash: bcrypt.hashSync(body.pwd, salt),
+    getCapcha(req, resp, next) {
+      const body = req.body;
+      const code = req.session.validCode || Math.random().toString().slice(-6);
+      const userName = req.session.user.userName;
+      req.session.validCode = code;
+      global.setTimeout(() => {
+        req.session.validCode = null;
+      }  , 5 * 60 * 1000);
+      const postData = {
+        mobile: body.phone,
+        message: `您的验证码为${code}请在5分钟内完成注册。【以太战舰】`
+      };
+      const content = querystring.stringify(postData);
+      const options = getSmsOption(content);
+      const smsReq = https.request(options, (res) => {
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+          if(chunk.error === 0) {
+            resp.success(SUCCESS);
+          }else {
+            resp.failed(chunk.msg);
+          }
         });
-        if(body.invitor) {
-          findInvitor = db.User.findById(body.invitor);
-        }
-        return Promise.all([newUser.save(), findInvitor]);
-      }
-    }).then(([user, invitor]) => {
-      if(invitor) {
-        invitor.invited.push(user.id);
-        return Promise.all([user, invitor.save()]);
-      }else {
-        return Promise.all([user]);
-      }
-    }).then(([user]) => {
-      resp.success(`${user.name}用户创建成功！`);
-    }).catch(err => {
-      next(err);
-    });
-  },
-
-  login(req, resp, next) {
-    const body = req.body;
-    db.User.findOne({
-      name: body.name,
-    }).then(user => {
-      if(!user) {
-        resp.failed(`不存在的账户！`);
-      }else {
-        // TODO 添加校验码比较
-        if(bcrypt.compareSync(body.pwd, user.hash)) {
-          req.session.user = user.toJSON();
-          resp.success('登录成功!');
-        }else {
-          resp.failed('密码不正确！');
-        }
-      }
-    }).catch(err => {
-      next(err);
-    })
-  },
-
-  getCapcha(req, resp, next) {
-    const body = req.body;
-    const code = Math.random().toString().slice(-6);
-    // TODO 验证码存入数据库以及定时删除
-    const postData = {
-      mobile: body.phone,
-      message: `您的验证码为${code}请在5分钟内完成注册。【以太战舰】`
-    };
-    const content = querystring.stringify(postData);
-    const options = getSmsOption(content);
-    const smsReq = https.request(options, (res) => {
-      res.setEncoding('utf8');
-      res.on('data', (chunk) => {
-        if(chunk.error === 0) {
-          resp.success(SUCCESS);
-        }else {
-          resp.failed(chunk.msg);
-        }
+        res.on('end', () => {
+          console.log('over');
+        });
       });
-      res.on('end', () => {
-        console.log('over');
-      });
-    });
-    smsReq.write(content);
-    smsReq.end();
-  },
+      smsReq.write(content);
+      smsReq.end();
+    },
 
-}
+  }
 
-module.exports = userController;
+  module.exports = userController;
